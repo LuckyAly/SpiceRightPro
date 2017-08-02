1c85bfc71441464ff31590fd65b82a230ec5bf9e
LuckyAly <Lucky.ali.k@gmail.com>
2017-08-02 12:38:16 -0400

    Initial check-in of module SpiceRight

---
+++ SpiceRight/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SpiceRight
+{
+	public class HomeViewModel : ViewModelBase
+	{
+		public HomeViewModel(Page page) : base(page)
+		{
+			Title = "My Shoppe";
+		}
+
+
+	}
+}
